a333b00e6c0bea27cc8609897dc929b4408b7f76
Crowdbotics <team@crowdbotics.com>
2020-11-17 03:23:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/drunkometer_22690/MainActivity.java
@@ -0,0 +1,15 @@
+package com.drunkometer_22690;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "drunkometer_22690";
+    }
+}
